f5de2e2732d3eab5d8c594f4fe3dc9db78a4c5c6
Chinmay Mohite <chinmaymohite0@gmail.com>
2026-08-14 23:42:46 +0530

    Day 12 completed

---
+++ Leetcode/Arrays/Solutions/169_Majority-Element.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int el = 0;
+        int count = 0;
+        
+        for (int num : nums) {
+            if (count == 0) {
+                el = num;
+            }
+            
+            if (num == el) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        
+        return el;
+    }
+}
