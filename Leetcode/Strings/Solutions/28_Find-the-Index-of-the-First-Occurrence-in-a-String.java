5db34ea0d526a3b907379e6b0510e55976646c5b
Chinmay Mohite <chinmaymohite0@gmail.com>
2026-08-27 22:00:31 +0530

    Day 25 Done

---
+++ Leetcode/Strings/Solutions/28_Find-the-Index-of-the-First-Occurrence-in-a-String.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        int hLen = haystack.length();
+        int nLen = needle.length();
+        
+        for (int i = 0; i <= hLen - nLen; i++) {
+            
+            String sub = haystack.substring(i, i + nLen);
+            
+            if (sub.equals(needle)) {
+                return i; 
+            }
+        }
+        
+        return -1; 
+    }
+}
