223057c1fc77a36c6138ce721d731320d7f8405c
Chinmay Mohite <chinmaymohite0@gmail.com>
2026-09-11 23:41:52 +0530

    Day 61 updated

--- Codeforces/630A_Again-Twenty-Five.java
+++ Codeforces/630A_Again-Twenty-Five.java
@@ -1,17 +1,13 @@
-import java.util.Scanner;
-import java.util.HashSet;
 
-public class DoNotBeDistracted {
+import java.util.Scanner;
+ 
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        int n = sc.nextInt();
+        long n = sc.nextLong();
         
-        int result = (int) Math.pow(5, n);
-        
-        int digits = result % 100;
-        
-        System.out.println(digits);
+        System.out.println(25);
         
         sc.close();
     }
