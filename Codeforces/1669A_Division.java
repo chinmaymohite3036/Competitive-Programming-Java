814f90b2084d963456bf073630871c30cac23f33
Chinmay Mohite <chinmaymohite0@gmail.com>
2026-09-21 23:50:01 +0530

    Day 71 update

--- Codeforces/1669A_Division.java
+++ Codeforces/1669A_Division.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.util.System.in);
+        Scanner sc = new Scanner(System.in);
         
         // Read the number of test cases
         int t = sc.nextInt();
